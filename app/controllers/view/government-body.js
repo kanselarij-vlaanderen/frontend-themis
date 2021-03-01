e070d4d23adcc207b174038a70cc49652542b991
Annelies-P <annelies.pauwels@gmail.com>
2021-03-01 10:10:18 +0100

    group by persons in controller

--- app/controllers/view/government-body.js
+++ app/controllers/view/government-body.js
@@ -3,4 +3,24 @@ import Controller from '@ember/controller';
 export default class ViewGovernmentbodyController extends Controller {
   queryParams = ['resource'];
 
+  get mandateesByName() {
+    const persons = [];
+
+    this.model.mandatees.forEach(function(mandatee) {
+      let person = persons.findBy('id', mandatee.person.get('id'));
+
+      if (!person) {
+        person = {
+            id: mandatee.person.get('id'),
+            mandatees: [],
+            person: mandatee.person
+         };
+         persons.push(person);
+      }
+
+      person.mandatees.push(mandatee);
+   });
+
+   return persons;
+  }
 }
