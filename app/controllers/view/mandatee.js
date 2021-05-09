3e78ed2d581c257f3bb733c0fe64e47352da685c
Annelies-P <annelies.pauwels@gmail.com>
2021-05-09 19:12:53 +0200

    toevoegen nieuwsberichten mandataris

--- app/controllers/view/mandatee.js
+++ app/controllers/view/mandatee.js
@@ -1,6 +1,47 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { task } from 'ember-concurrency-decorators';
+import { tracked } from '@glimmer/tracking';
+
 
 export default class ViewMandateeController extends Controller {
-  queryParams = ['resource'];
+  queryParams = ['resource', 'page', 'size'];
+
+  @service store;
+
+  @tracked newsItems;
+  @tracked resource;
+  @tracked page = 0;
+  @tracked size = 10;
+
+  constructor() {
+    super(...arguments);
+    this.getNewsItems.perform();
+  }
+
+  @task
+  *getNewsItems() {
+    console.log(this.queryParams);
+    this.newsItems = yield this.store.query('news-item',
+      { filter:
+        {
+          mandatees: {
+            ':uri:': this.resource
+          }
+        },
+      sort: '-date',
+      page: {
+        size: this.size,
+        number: this.page
+      }});
+      console.log(this.newsItems)
+  }
+
+  @action
+  async selectPage(page) {
+    this.page = page;
+    await this.getNewsItems.perform();
+  }
 
 }
