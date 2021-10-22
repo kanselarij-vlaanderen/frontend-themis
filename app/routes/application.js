79c2efe42accf6a34e49a3b4dbb3ffdfef30ef6c
Michaël Dierick <michael.dierick@redpencil.io>
2021-10-22 15:53:44 +0200

    fix copy-paste mistake + document try-catch strategy

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,6 +4,7 @@ import ENV from 'frontend-themis/config/environment';
 export default class ApplicationRoute extends Route {
   headTags() {
     const headers = [];
+    // try ... catch tests URL-validity to see if the EMBER_... ENV-var placeholder has been substituted
     try {
       new URL(ENV.VO_HEADER_WIDGET_URL);
       headers.push({
@@ -19,7 +20,7 @@ export default class ApplicationRoute extends Route {
       new URL(ENV.VO_FOOTER_WIDGET_URL);
       headers.push({
         type: 'script',
-        tagId: 'vo-header-widget',
+        tagId: 'vo-footer-widget',
         attrs: {
           name: 'src',
           content: ENV.VO_FOOTER_WIDGET_URL
