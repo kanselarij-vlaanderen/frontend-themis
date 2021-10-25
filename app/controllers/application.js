3815e35f55cad21a188885f74bc5ab7838a32b86
Michaël Dierick <michael.dierick@redpencil.io>
2021-10-25 10:46:43 +0200

    dynamically inject widget through vo widget api

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,9 @@ import Controller from '@ember/controller';
 import ENV from 'frontend-themis/config/environment';
 
 export default class ApplicationController extends Controller {
+  VO_HEADER_WIDGET_URL = ENV.VO_HEADER_WIDGET_URL;
+  VO_FOOTER_WIDGET_URL = ENV.VO_FOOTER_WIDGET_URL;
+
   get hasExternallySourcedHeader() {
     try {
       new URL(ENV.VO_HEADER_WIDGET_URL);
