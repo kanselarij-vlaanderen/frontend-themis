df99d1ee5ee1c32fe08cde707d37bfcb0c3cb076
Karel Persoons <karel@brenner.company>
2025-09-03 18:48:47 +0200

    KAS-5067 Update ember from v4.8.0 to v5.12.0
    # Conflicts:
    #	app/styles/app.css
    #	app/templates/application.hbs
    #	ember-cli-build.js

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -16,19 +16,6 @@ module.exports = function (defaults) {
     },
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
   app.import('node_modules/@triply/yasgui/build/yasgui.min.css');
 
   // https://vlaamseoverheid.atlassian.net/wiki/spaces/IKPubliek/pages/2327449182/Technische+documentatie+Widget-platform#Browser-Polyfills
