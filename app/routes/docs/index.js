e04a2274c5a0fdc20b1a855f537b0072905640b5
Sven Valenberghs <sven.valenberghs@faros.be>
2025-09-04 13:22:58 +0200

    KAS-5067 fix various deprecations, await in template for promises

--- app/routes/docs/index.js
+++ app/routes/docs/index.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class DocsIndexRoute extends Route {
+  @service router;
+
   beforeModel() {
-    this.transitionTo('docs.publication');
+    this.router.transitionTo('docs.publication');
   }
 }
