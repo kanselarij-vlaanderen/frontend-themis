20bd4fe63259c9ad2c725ca8488a262629f8a9dc
Karel Persoons <karel@brenner.company>
2025-09-18 16:51:59 +0200

    KAS-5067 Add comment within Fastboot config file regarding local development with proxy

--- config/fastboot.js
+++ config/fastboot.js
@@ -2,8 +2,8 @@ module.exports = function(environment) {
   return {
     buildSandboxGlobals(defaultGlobals) {
       return Object.assign({}, defaultGlobals, {
-        // Enable BACKEND_URL and set to http://host (or http://localhost)
-        // for local development.
+        // Enable BACKEND_URL and set to http://host (or http://localhost) for local development.
+        // or https://themis-test.vlaanderen.be if you use the proxied version
         BACKEND_URL: 'http://host/',
 
         // Fastboot support for Ember Data >= 4.12
